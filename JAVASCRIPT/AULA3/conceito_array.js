1be986b5b698d40b172fd68ab760f79bfaa162c9
Kauan Viana <kauanvianasoares10@gmail.com>
2026-09-25 09:30:09 -0300

    Back End

---
+++ JAVASCRIPT/AULA3/conceito_array.js
@@ -0,0 +1,31 @@
+//Array
+//Um array é uma lista que pode armazenar varios valores
+
+let frutas = [" Banana" , " Maçã ", " Manga"];
+
+//let frutas = ["0", "1", "2"]
+ 
+console.log(frutas[0]);
+console.log(frutas[1]);
+console.log(frutas[2]);
+
+
+
+
+// Podemos adicionar itens", sem alterar o array diretamente
+
+frutas.push("Laranja"); //Adiciona ao final 
+
+console.log(frutas[3]);
+
+frutas.pop(); //remove do final (no caso a laranja do [3])
+
+console.log(frutas);
+
+// - Crie  um array chamado `animais` e adicione três animais animais.
+// - Exiba o primeiro e o último no console
+
+let animais = ["Cachorro", "Macaco", "Babuino"];
+console.log(animais[0]);
+console.log(animais[2]);
+    
---
+++ JAVASCRIPT/AULA3/conceitos_objetos.js
@@ -0,0 +1,25 @@
+// Object (Objetos)
+// Um objeto agrupa varias informações dentro de uma variavel, usando chave e valor
+
+let pessoa = {
+    nome: "Viana",
+    idade: 16,
+    cidade : "São Paulo"
+};
+
+console.log(pessoa.nome);
+console.log(pessoa.idade);
+console.log(pessoa.cidade);
+
+// - Crie um objeto chamado `carro` com as propriedades `marca`, `modelo` e `ano`
+
+let carro = {
+   Marca: "Volkswagen",
+   Modelo: "T-Cross",
+   Ano: 2027
+}:
+
+console.log(carro.Marca)
+console.log(carro.Modelo)
+console.log(carro.Ano)
+
